2245e51941299048e5f1d8aae725bf9995efd6a4
Alessandro Menduni <menduni6@gmail.com>
2015-10-24 20:26:22 +0200

    Adjust equality operators according to linter

--- numericInput.js
+++ numericInput.js
@@ -8,87 +8,80 @@
 ///////////////////////////////////////////////////////////////
 
 //	Sets a keypress event for the selected element allowing only numbers. Typically this would only be bound to a textbox.
-(function( $ ) {
-	// Plugin defaults
-	var defaults = {
-		allowFloat: false,
-		allowNegative: false
-	};	
-	
-	// Plugin definition
-	//	allowFloat: (boolean) Allows floating point (real) numbers. If set to false only integers will be allowed. Default: false.
-	//	allowNegative: (boolean) Allows negative values. If set to false only positive number input will be allowed. Default: false.
- 	$.fn.numericInput = function( options ) { 
-		var settings = $.extend( {}, defaults, options ); 
-		var allowFloat = settings.allowFloat;
-		var allowNegative = settings.allowNegative;
-		
-		this.keypress(function (event) {
-			var inputCode = event.which;
-			var currentValue = $(this).val();
+(function($) {
+    // Plugin defaults
+    var defaults = {
+        allowFloat: false,
+        allowNegative: false
+    };
 
-			if (inputCode > 0 && (inputCode < 48 || inputCode > 57))	// Checks the if the character code is not a digit
-			{
-				if (allowFloat == true && inputCode == 46)	// Conditions for a period (decimal point)
-				{
-					//Disallows a period before a negative
-					if (allowNegative == true && getCaret(this) == 0 && currentValue.charAt(0) == '-') 
-						return false;
+    // Plugin definition
+    //	allowFloat: (boolean) Allows floating point (real) numbers. If set to false only integers will be allowed. Default: false.
+    //	allowNegative: (boolean) Allows negative values. If set to false only positive number input will be allowed. Default: false.
+    $.fn.numericInput = function(options) {
+        var settings = $.extend({}, defaults, options);
+        var allowFloat = settings.allowFloat;
+        var allowNegative = settings.allowNegative;
 
-					//Disallows more than one decimal point.
-					if (currentValue.match(/[.]/)) 
-						return false; 
-				}
+        this.keypress(function(event) {
+            var inputCode = event.which;
+            var currentValue = $(this).val();
 
-				else if (allowNegative == true && inputCode == 45)	// Conditions for a decimal point
-				{
-					if(currentValue.charAt(0) == '-') 
-						return false;
-					
-					if(getCaret(this) != 0) 
-						return false; 
-				}
+            if (inputCode > 0 && (inputCode < 48 || inputCode > 57)) // Checks the if the character code is not a digit
+            {
+                if (allowFloat === true && inputCode == 46) // Conditions for a period (decimal point)
+                {
+                    //Disallows a period before a negative
+                    if (allowNegative === true && getCaret(this) === 0 && currentValue.charAt(0) == '-')
+                        return false;
 
-				else if (inputCode == 8) 	// Allows backspace
-					return true; 
+                    //Disallows more than one decimal point.
+                    if (currentValue.match(/[.]/))
+                        return false;
+                } else if (allowNegative === true && inputCode == 45) // Conditions for a decimal point
+                {
+                    if (currentValue.charAt(0) == '-')
+                        return false;
 
-				else								// Disallow non-numeric
-					return false;  
-			}
+                    if (getCaret(this) !== 0)
+                        return false;
+                } else if (inputCode == 8) // Allows backspace
+                    return true;
 
-			else if(inputCode > 0 && (inputCode >= 48 && inputCode <= 57))	// Disallows numbers before a negative.
-			{
-				if (allowNegative == true && currentValue.charAt(0) == '-' && getCaret(this) == 0) 
-					return false;
-			}
-		});
-		
-		return this;
-	};
-	
-	
-	// Private function for selecting cursor position. Makes IE play nice.
-	//	http://stackoverflow.com/questions/263743/how-to-get-caret-position-in-textarea
-	function getCaret(element) 
-	{ 
-		if (element.selectionStart) 
-			return element.selectionStart; 
+                else // Disallow non-numeric
+                    return false;
+            } else if (inputCode > 0 && (inputCode >= 48 && inputCode <= 57)) // Disallows numbers before a negative.
+            {
+                if (allowNegative === true && currentValue.charAt(0) == '-' && getCaret(this) === 0)
+                    return false;
+            }
+        });
 
-		else if (document.selection) //IE specific
-		{ 
-			element.focus(); 
+        return this;
+    };
 
-			var r = document.selection.createRange(); 
-			if (r == null) 
-				return 0; 
 
-			var re = element.createTextRange(), 
-			rc = re.duplicate(); 
-			re.moveToBookmark(r.getBookmark()); 
-			rc.setEndPoint('EndToStart', re); 
-			return rc.text.length; 
-		}  
+    // Private function for selecting cursor position. Makes IE play nice.
+    //	http://stackoverflow.com/questions/263743/how-to-get-caret-position-in-textarea
+    function getCaret(element) {
+        if (element.selectionStart)
+            return element.selectionStart;
 
-		return 0; 
-	};
-}( jQuery ));
+        else if (document.selection) //IE specific
+        {
+            element.focus();
+
+            var r = document.selection.createRange();
+            if (r === null)
+                return 0;
+
+            var re = element.createTextRange(),
+                rc = re.duplicate();
+            re.moveToBookmark(r.getBookmark());
+            rc.setEndPoint('EndToStart', re);
+            return rc.text.length;
+        }
+
+        return 0;
+    }
+}(jQuery));
