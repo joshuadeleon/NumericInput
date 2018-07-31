affe97f9fd2de766b38bbd516535391298a929e5
kaizer1v <kaizer1v@gmail.com>
2018-07-31 20:47:26 +0530

    using regex to validate number types on blur event

--- numericInput.js
+++ numericInput.js
@@ -1,5 +1,6 @@
 ///////////////////////////////////////////////////////////////
-//  Author: Joshua De Leon
+//  Original Author: Joshua De Leon
+//  Modified By: Vivek Shrinivasan
 //  File: numericInput.js
 //  Description: Allows only numeric input in an element.
 //  
@@ -13,9 +14,10 @@
   var defaults = {
     allowFloat: false,
     allowNegative: false,
-    min: undefined,
-    max: undefined
+    min: 0,
+    max: 1
   };
+  var ascii_digits = [48, 57];
 
   // Plugin definition
   //  allowFloat: (boolean) Allows floating point (real) numbers. If set to false only integers will be allowed. Default: false.
@@ -23,93 +25,86 @@
   //  min: (int/float) If set, when the user leaves the input if the entered value is too low it will be set to this value
   //  max: (int/float) If set, when the user leaves the input if the entered value is too high it will be set to this value
   $.fn.numericInput = function(options) {
+    var $this = this;
     var settings = $.extend({}, defaults, options);
     var allowFloat = settings.allowFloat;
     var allowNegative = settings.allowNegative;
     var min = settings.min;
     var max = settings.max;
+    var pattern = /^-?\d+\.?\d+$/;
 
-    if (min == max) {
-      throw ("The minimum value cannot be the same as the max value");
-    } else if (min > max) //If the values are swapped we swap them back
-    {
+    if(min == max) {
+      throw ('The minimum value cannot be the same as the max value');
+    } else if(min > max) { //If the values are swapped we swap them back
       var temp = min;
       min = max;
       max = temp;
     }
 
-    this.keypress(function(event) {
-      var inputCode = event.which;
-      var currentValue = $(this).val();
+    // beautify the input type
+    // add a class
+    $this.addClass('numericInput');
 
-      if (inputCode > 0 && (inputCode < 48 || inputCode > 57)) // Checks the if the character code is not a digit
-      {
-        if (allowFloat == true && inputCode == 46) // Conditions for a period (decimal point)
-        {
+    // attach an event of `keypress` on the element
+    $this.on('keypress', function(event) {
+      var inputCode = event.which;
+      var currentValue = $($this).val();
+
+      if(inputCode > 0 && (inputCode < ascii_digits[0] || inputCode > ascii_digits[1])) {
+        // Checks the if the character code is not a digit
+        if(allowFloat == true && inputCode == 46) {
+          // Conditions for a period (decimal point)
           //Disallows a period before a negative
-          if (allowNegative == true && getCaret(this) == 0 && currentValue.charAt(0) == '-')
+          if(allowNegative == true && getCaret($this) == 0 && currentValue.charAt(0) == '-')
             return false;
 
           //Disallows more than one decimal point.
-          if (currentValue.match(/[.]/))
+          if(currentValue.match(/[.]/))
             return false;
-        } else if (allowNegative == true && inputCode == 45) // Conditions for a decimal point
-        {
-          if (currentValue.charAt(0) == '-')
+        } else if(allowNegative == true && inputCode == 45) { // Conditions for a decimal point
+          if(currentValue.charAt(0) == '-')
             return false;
 
-          if (getCaret(this) != 0)
+          if(getCaret($this) != 0)
             return false;
-        } else if (inputCode == 8 || inputCode == 67 || inputCode == 86) // Allows backspace , ctrl+c ,ctrl+v (copy & paste)
+        } else if(inputCode == 8 || inputCode == 67 || inputCode == 86) {
+          // Allows backspace , ctrl+c ,ctrl+v (copy & paste)
           return true;
-
-        else // Disallow non-numeric
+        } else {
+          // Disallow non-numeric
           return false;
-      } else if (inputCode > 0 && (inputCode >= 48 && inputCode <= 57)) // Disallows numbers before a negative.
-      {
-        if (allowNegative == true && currentValue.charAt(0) == '-' && getCaret(this) == 0)
+        }
+      } else if(inputCode > 0 && (inputCode >= ascii_digits[0] && inputCode <= ascii_digits[1])) { // Disallows numbers before a negative.
+        if(allowNegative == true && currentValue.charAt(0) == '-' && getCaret($this) == 0)
           return false;
       }
     });
 
+    // attach `blur` even
+    $this.on('blur', function(event) {
+      var val = $(this).val();
+      var change = min;
+      var passed = pattern.test(val);
 
-    this.blur(function(event) {
-      //Get and store the current value
-      var currentValue = $(this).val();
-
-      //If the value isn't empty
-      if (currentValue.length > 0) {
-        //Get the float value, even if we're not using floats this will be ok
-        var floatValue = parseFloat(currentValue);
-
-        //If min is specified and the value is less set the value to min
-        if (min !== undefined && floatValue < min) {
-          $(this).val(min);
-        }
-
-        //If max is specified and the value is less set the value to max
-        if (max !== undefined && floatValue > max) {
-          $(this).val(max);
-        }
-      }
+      // if value not in range between `min` and `max`, then limit it
+      if(passed) change = (val < min) ? min : (val > max) ? max : val;
+      $(this).val(change);
     });
 
-    return this;
+    return $this;
   };
-
 
   // Private function for selecting cursor position. Makes IE play nice.
   //  http://stackoverflow.com/questions/263743/how-to-get-caret-position-in-textarea
   function getCaret(element) {
-    if (element.selectionStart)
+    if(element.selectionStart)
       return element.selectionStart;
 
-    else if (document.selection) //IE specific
-    {
+    else if(document.selection) { //IE specific
       element.focus();
 
       var r = document.selection.createRange();
-      if (r == null)
+      if(r == null)
         return 0;
 
       var re = element.createTextRange(),
